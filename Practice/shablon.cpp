497ab457e5de5ca45fc6b71991b0b56abc14de57
Aer0m <47530448+Aer0m@users.noreply.github.com>
2019-02-11 19:59:28 +0300

    Create shablon.cpp

---
+++ Practice/shablon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+ 
+ setlocale(LC_ALL, "Russian"); //чтобы не поехал русский шрифт
+ 
+ cout << "Практическая работа №2"<<endl;
+ cout << "Выполнил(а) студент(ка) группы ИКБО-03-18 Имя Фамилия"<<endl;
+ }

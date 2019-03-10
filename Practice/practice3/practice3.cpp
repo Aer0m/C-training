4f5fb121529fcc8b1f6e000252e69d7705a695d0
Aer0m <47530448+Aer0m@users.noreply.github.com>
2019-03-11 01:52:39 +0300

    Create practice3.cpp

---
+++ Practice/practice3/practice3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class book
+{
+	string author;
+	string bookname;
+	int parts;
+	int pages;
+
+	
+};
+
+
+
+int main() {
+
+	setlocale(LC_ALL, "Russian")
+	
+	book Firstbook;
+	Firstbook.author = "Рэй Брэдбери";
+	Firstbook.bookname = "451 по Фаренгейту";
+	Firstbook.parts = 3;
+	Firstbook.pages = 246;
+	
+	cout<< Firstbook.author << endl;
+	cout<< Firstbook.bookname << endl;
+	cout<< Firstbook.parts << endl;
+	cout<< Firstbook.pages << endl;
+	
+	
+
+	return 0;
+}

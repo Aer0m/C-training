513c3cd8b92bfef5e7c99cefbb8b6654520dac41
Aer0m <47530448+Aer0m@users.noreply.github.com>
2019-03-11 11:28:50 +0300

    Update practice3.cpp

--- Practice/practice3/practice3.cpp
+++ Practice/practice3/practice3.cpp
@@ -19,17 +19,21 @@ class book
 
 int main() {
 
-	setlocale(LC_ALL, "Russian")
-	
+	setlocale(LC_ALL, "Russian");
+    
 	book Firstbook;
 	Firstbook.author = "Рэй Брэдбери";
 	Firstbook.bookname = "451 по Фаренгейту";
 	Firstbook.parts = 3;
 	Firstbook.pages = 246;
 	
+	cout<<"Автор книги: ";
 	cout<< Firstbook.author << endl;
+	cout<<"Название книги: ";
 	cout<< Firstbook.bookname << endl;
+	cout<<"Количество частей: ";
 	cout<< Firstbook.parts << endl;
+	cout<<"Количество страниц: ";
 	cout<< Firstbook.pages << endl;
 	
 	

1883f7857fc8441a636a63dc998c39b5e8e01ed3
Aer0m <47530448+Aer0m@users.noreply.github.com>
2019-03-18 12:15:04 +0300

    Create practice3_remake.cpp

---
+++ Practice/practice3/practice3_remake.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class book
+{
+    public:
+
+	string author;
+	string bookname;
+	int parts;
+	int pages;
+
+    void output()
+    {
+        cout<< "Количество частей: "<<parts<<endl;
+        cout << "Количество страниц: " << pages<<endl;
+        
+
+    }
+
+
+
+	
+};
+
+
+
+int main() {
+    string author1;
+	setlocale(LC_ALL, "Russian");
+    cout << "Практическая работа №3"<<endl;
+    cout << "Выполнил cтудент группы ИКБО-03-18 Кириенко Андрей"<<endl;
+
+cout <<"Введите автора ";
+cin >>author1;
+
+
+	book Firstbook {"Рэй","451 по Фаренгейту",3, 246};
+	book Secondbook {"М.Ю. Лермонтов", "Герой нашего времени",2, 150};
+	
+if (author1 == Firstbook.author) {
+    
+    Firstbook.output();
+
+    
+}
+	
+else if (author1 == Secondbook.author) {
+    
+    Secondbook.output();
+    
+}
+else cout << "Такой книги нет";
+	return 0;
+}
